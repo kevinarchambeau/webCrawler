c458abe9f6dc93a839e89885b0bf5a346d9d3c21
Kevin Archambeau <xxerexx@gmail.com>
2024-08-06 16:31:49 -0700

    implemented getURLsFromHTML function

--- crawl.js
+++ crawl.js
@@ -1,5 +1,7 @@
+import { JSDOM } from 'jsdom'
+
 function normalizeURL(url) {
-    let parsed = new URL(url)
+    const parsed = new URL(url)
     let normalized = parsed.host
 
     if (parsed.pathname.slice(-1) === "/") {
@@ -12,4 +14,20 @@ function normalizeURL(url) {
     return normalized
 }
 
-export { normalizeURL}
+function getURLsFromHTML (html, baseURL) {
+    const urls = []
+    const dom = new JSDOM(html)
+    const anchors = dom.window.document.querySelectorAll("a")
+    anchors.forEach((anchor) => {
+        if (anchor.host === "") {
+            urls.push(baseURL + anchor.href)
+        }
+        else {
+            urls.push(anchor.host + anchor.pathname)
+        }
+    })
+    console.log(urls)
+    return urls
+}
+
+export { normalizeURL, getURLsFromHTML}
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 import { test, expect } from "@jest/globals";
-import { normalizeURL } from "./crawl.js";
+import { normalizeURL, getURLsFromHTML } from "./crawl.js";
 
 test("url with ending /", () => {
     expect(normalizeURL("https://blog.boot.dev/path/")).toBe("blog.boot.dev/path")
@@ -7,4 +7,15 @@ test("url with ending /", () => {
 
 test("url", () => {
     expect(normalizeURL("https://blog.boot.dev/path")).toBe("blog.boot.dev/path")
+})
+
+test("getURLsFromHTML basic", () => {
+    let html = "<html>\n" +
+        "    <body>\n" +
+        "        <a href=\"https://blog.boot.dev\"><span>Go to Boot.dev</span></a>\n" +
+        "        <a href=\"/something/wee\"><span>relative</span></a>\n" +
+        "    </body>\n" +
+        "</html>"
+    const expected = ["blog.boot.dev/", "blogs.boot.dev/something/wee"]
+    expect(getURLsFromHTML(html, "blogs.boot.dev")).toEqual(expect.arrayContaining(expected))
 })
